1f9fdc5611bc74a7aeae372e2f0ba04d35cbb7ed
Lucas Toupitzen <lucastferracin@gmail.com>
2023-01-09 16:58:42 -0300

    construção do utilitário botão azul

---
+++ projetotreinee/src/components/utilities/BotaoAzul.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function BotaoAzul(props) {
+    return (
+        <div className="botao-azul">
+            <i className={props.icone}></i>
+            <p className="texto-botao-azul">{props.texto}</p>
+        </div>
+    )
+}
+
+/*Nota para facilitar o uso
+Caso nescessitar do icone de adicionar usuário em seu componente, passar como 
+parâmetro na chamada do botão icone="fa-solid fa-user-plus". 
+*/
--- projetotreinee/src/index.js
+++ projetotreinee/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import BotaoAzul from './components/utilities/BotaoAzul';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    
+
 );
 
