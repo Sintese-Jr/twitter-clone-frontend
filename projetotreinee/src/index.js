7399a0d1ad0c9b6beec48218ed74ebd4c8731c28
Lucas Toupitzen <lucastferracin@gmail.com>
2023-01-10 12:09:30 -0300

    mudança do arquivo css do Botao Azul

--- projetotreinee/src/index.js
+++ projetotreinee/src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import BotaoAzul from './components/utilities/BotaoAzul';
-import './index.css';
+import './styles/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
+    
 );
 
