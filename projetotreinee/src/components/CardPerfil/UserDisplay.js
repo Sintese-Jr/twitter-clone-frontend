7a28ff077ed17775fd0af269e394865017a22067
Lucas Toupitzen <lucastferracin@gmail.com>
2023-01-10 20:05:29 -0300

    Entrega do componente CardPerfil

---
+++ projetotreinee/src/components/CardPerfil/UserDisplay.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../../styles/cardperfil.css"
+import Description from "./UserDisplayComponents/Description";
+import ImagemPerfil from "./UserDisplayComponents/ImagemPerfil";
+import InfoBar from "./UserDisplayComponents/InfoBar";
+
+export default function UserDisplay(props) {
+    
+    return (
+        <div className="userdisplay">
+            <ImagemPerfil />
+            <InfoBar
+                nomeuser="Daniela Jensen"
+                followingnomeuser="1,370"
+                followersuser="10,5k"
+            />
+            <Description descricao="Sempre é um belo dia para uma boa leitura"/>
+        </div>
+    );
+}
