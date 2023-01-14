ba8994b13e990b4f9b42775e3d2f4444aaf30a70
Lucas Toupitzen <lucastferracin@gmail.com>
2023-01-14 16:58:18 -0300

    feat: ajuste dos componentes ao css reset

--- projetotreinee/src/index.js
+++ projetotreinee/src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    
+   
 );
 
