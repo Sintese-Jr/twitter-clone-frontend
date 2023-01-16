095bdf953803eedc88c539485e40c07e3df6ddf7
guga7895 <guga7894@gmail.com>
2023-01-16 12:03:24 -0300

    Correção e adição do menu lateral

---
+++ projetotreinee/src/components/Menulateral/Menulateral.js
@@ -0,0 +1,24 @@
+import './Menulateral.css';
+import React from 'react';
+
+function Menulateral(props) {
+  return (
+    <div class="container">
+        <div class="container2">
+            <p class="canto"></p>
+            <p class="tweets">{props.primeiro}</p>  
+        </div>
+        <div class="container2">
+            <p class="tweets2">{props.segundo}</p>
+        </div>  
+        <div class="container2">
+            <p class="tweets2">{props.terceiro}</p>
+        </div>
+        <div class="container2">
+            <p class="tweets2">{props.quarto}</p>
+        </div>
+    </div>
+  );
+}
+
+export default Menulateral;
