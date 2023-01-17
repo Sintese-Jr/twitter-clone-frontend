15d791bbedca453390afd7a0c3c3bbbaac2faf9b
Matheus Salermo <matheussalermo11@gmail.com>
2023-01-17 17:45:06 -0300

    Estrtura do Tweetar adicionada

--- projetotreinee/src/index.js
+++ projetotreinee/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
-import Trends from './components/Trends/trends.js';
+import Tweetar from './components/Tweetar/Tweetar.js';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
+    <Tweetar></Tweetar>
 );
 
