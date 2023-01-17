e85ad1c6e611ee3b475d075979912c08c1cd5d07
Matheus Salermo <matheussalermo11@gmail.com>
2023-01-17 15:18:35 -0300

    Adicionando a estrutura do Tweet!!

--- projetotreinee/src/index.js
+++ projetotreinee/src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import QuemSeguir from './components/QuemSeguir/QuemSeguir.js';
+import Tweet from './components/Tweet/Tweet.js';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
+    <Tweet></Tweet>
 );
 
