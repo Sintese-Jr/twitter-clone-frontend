48f23ab4d33a29eb5278275c8aa1279f706fab52
Lucas Ferracin <lucastferracin@gmail.com>
2023-01-20 11:45:33 -0300

    finalização da página de perfil de modo estático

--- projetotreinee/src/index.js
+++ projetotreinee/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
-
+import PerfilPage from "./pages/Perfil"
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-   
+   <PerfilPage />
 );
 
