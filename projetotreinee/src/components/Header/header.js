e23bba8a6f71285412a80a9b4485ff0a1c057b83
Vinicius Gabriel de Souza Pereira <vinigabri.pereira@gmail.com>
2023-01-20 23:12:49 -0300

    Header interativo
    O Header já está interativo porém ainda não necessários ajustes

--- projetotreinee/src/components/Header/header.js
+++ projetotreinee/src/components/Header/header.js
@@ -4,9 +4,35 @@ import LogoTelaPequena from './images/tweeter-small.svg'
 import FotoPerfil from './images/perfil.jpg'
 import Seta from './images/seta.png'
 import '../../styles/header.css'
-import {Link} from "react-router-dom"
+import SetaCima from './images/setaCima.png'
+import MenuPerfil from '../MenuPerfil/MenuPerfil'
 
 function Header() {
+
+    function abrir() {
+        const menuPerfil = document.getElementById("menuPerfil");
+
+        const setaBaixo = document.getElementById("seta");
+        setaBaixo.classList.remove('aparecerSeta');
+        setaBaixo.classList.add('desaparecerSeta');
+        menuPerfil.classList.remove('desaparecerMenu');
+        const setaCima = document.getElementById("setaCima");
+        setaCima.classList.remove('desaparecerSeta');
+        setaCima.classList.add('aparecerSeta');
+    }
+
+    function fechar() {
+        const menuPerfil = document.getElementById("menuPerfil");
+
+        const setaCima = document.getElementById("setaCima");
+        setaCima.classList.remove('aparecerSeta');
+        setaCima.classList.add('desaparecerSeta');
+        menuPerfil.classList.add('desaparecerMenu');
+        const setaBaixo = document.getElementById("seta");
+        setaBaixo.classList.remove('desaparecerSeta');
+        setaBaixo.classList.add('aparecerSeta');
+    }
+    
     return(
         <header>
             <div className='conteudo'>
@@ -14,9 +40,9 @@ function Header() {
                 <img src={LogoTelaPequena} alt="Logotipo Tweeter" className="logoPequena"></img>
                 <table className='paginas'>
                     <tr>
-                        <td className='tdespaco home'><Link to="/">Home</Link></td>
-                        <td className='tdespaco explore'><Link to="/explore">Explore</Link></td>
-                        <td className='tdespaco bookmarks' id='book'><Link to="/bookmarks">Bookmarks</Link></td>
+                        <td className='tdespaco'>Home</td>
+                        <td className='tdespaco'>Explore</td>
+                        <td className='tdespaco' id='book'>Bookmarks</td>
                     </tr>
                 </table>
 
@@ -24,9 +50,13 @@ function Header() {
                     <tr>
                         <td className='tdespacoPerfil'><img src={FotoPerfil} alt="Foto Perfil" id="fotoPerfil"></img></td>
                         <td className='tdespacoPerfil' id="nomeUsuario">Nome do usuário</td>
-                        <td className='tdespacoPerfil'><img src={Seta} alt="Seta para baixo" id="seta"></img></td>
+                        <td className='tdespacoPerfil'><img src={Seta} alt="Seta para baixo" id="seta" className='aparecerSeta' onClick={abrir}></img></td>
+                        <td className='tdespacoPerfil'><img src={SetaCima} alt="Seta para cima" id="setaCima" className='desaparecerSeta' onClick={fechar}></img></td>
                     </tr>
                 </table>
+            </div>
+            <div id='menuPerfil' className='desaparecerMenu'>
+                <MenuPerfil></MenuPerfil>
             </div>
         </header>
     )
