47812bdff79c8ef626e321dc1464e20c737fa0af
andrepalaci0 <andrepalacio14@gmail.com>
2023-01-24 03:23:26 -0300

    Adicionada interatividade no botao do perfil

--- projetotreinee/src/components/utilities/BotaoAzul.js
+++ projetotreinee/src/components/utilities/BotaoAzul.js
@@ -3,7 +3,7 @@ import "../../styles/botaoAzul.css"
 
 export default function BotaoAzul(props) {
     return (
-        <div className="botao-retweet">
+        <div className="botao-azul">
             <i className={props.icone}></i>
             <p className="texto-botao-retweet">{props.texto}</p>
         </div>
