8f7b3ab6e56aa7c29b123567a1c61f9dfd86a915
Lucas Toupitzen <lucastferracin@gmail.com>
2023-01-25 11:00:36 -0300

    primeiros passos da mockagem

--- projetotreinee/src/pages/Home.js
+++ projetotreinee/src/pages/Home.js
@@ -2,12 +2,28 @@ import React from "react";
 import "../styles/HomePage.css"
 import Header from "../components/Header/header";
 import Trends from "../components/Trends/trends"
-import Tweet from "../components/Tweet/Tweet.js"
+import Tweet from "../components/Tweet/NewTweet.js"
 import RetweetTag from "../components/utilities/RetweetTag";
-import QuemSeguir from "../components/QuemSeguir/QuemSeguir"
+import QuemSeguir from "../components/QuemSeguir/QuemSeguir";
 import Tweetar from "../components/Tweetar/tweetar"
+import users from "../mockData/users.json";
+import tweetsDB from "../mockData/tweetsDB.json";
 
 export default function HomePage() {
+
+    const tweetsRecomendados = [...users["root"].tweetsFollowing];
+    const listaTweets = tweetsRecomendados.map(tweetId => {
+        const usuario = users[tweetsDB[tweetId].writerId];
+        const dados = {...tweetsDB[tweetId], 
+            "nome": usuario.nome,
+            "fotoPerfil": usuario.fotoPerfil
+        }
+        return <Tweet 
+            key={tweetId}
+            {...dados}
+        />
+    });
+
     return (
         <body>
             <Header />
@@ -15,12 +31,11 @@ export default function HomePage() {
                 <div className="tweet-tamanho-home">
                     <Tweetar />
                     <RetweetTag nome="Daniela Jensen" />
-                    <Tweet />
-                    <Tweet />
-                    <Tweet />
+                    {listaTweets}
                 </div>
                 <div className="lateral-esquerda">
-                    <Trends />
+                    <Trends 
+                    trends={users["root"].trends}/>
                     <div className="quem-seguir">
                         <QuemSeguir />
                     </div>
