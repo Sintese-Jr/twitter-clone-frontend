65ea2eef1ec0739cc27724bf3c13bb08045e40e2
Lucas Ferracin <lucastferracin@gmail.com>
2023-01-25 15:08:28 -0300

    correcao index.js

--- projetotreinee/src/index.js
+++ projetotreinee/src/index.js
@@ -3,14 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import { BrowserRouter } from 'react-router-dom'
 import App from './App';
-import WhoCanReply from './components/WhoCanReply/WhoCanReply';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-   <WhoCanReply>
-
-   </WhoCanReply>
+   <BrowserRouter>
+       <App />
+   </BrowserRouter>
 );
 
