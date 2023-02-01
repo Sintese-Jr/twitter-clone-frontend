870ef9a66d4bf05a7180ff0f7966f14a52f6e140
guga7895 <guga7894@gmail.com>
2023-02-01 16:27:31 -0300

    Ajustes (talvez finais) da responsividade do cardperfil

--- projetotreinee/src/index.js
+++ projetotreinee/src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import { BrowserRouter } from 'react-router-dom'
-import App from './App'
+import App from './App';
+import PerfilPage from './pages/Perfil';
+
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
    <BrowserRouter>
-       <App/>
+       <PerfilPage/>
    </BrowserRouter>
 );
 
