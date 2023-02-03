3222eb59ea880a27d88411626b8877fdddb93ea4
Lucas Ferracin <80696823+lucastoupitzen@users.noreply.github.com>
2023-02-03 14:49:02 -0300

    Update index.js

--- projetotreinee/src/index.js
+++ projetotreinee/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import { BrowserRouter } from 'react-router-dom'
 import App from './App';
-import PerfilPage from './pages/Perfil';
 
 
 
