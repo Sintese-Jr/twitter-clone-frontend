0f6b19ad0533d459c04957262c49f3f1b116c7f8
Matheus Rocha <01devmath@gmail.com>
2023-02-07 18:42:42 -0300

    feat: add who can reply option modal

--- projetotreinee/src/components/WhoCanReply/Caixas.js
+++ projetotreinee/src/components/WhoCanReply/Caixas.js
@@ -2,8 +2,12 @@ import "../../styles/caixas.css";
 import React from "react";
 
 export default function Caixas({ handleClick, option }) {
+  function changeOption() {
+    handleClick(option);
+  }
+
   return (
-    <div className="Caixas" onClick={handleClick(option)}>
+    <div className="Caixas" onClick={changeOption}>
       <div className="Everyone">
         <i class={"fa-solid " + option.icon}></i>
         {option.label}
