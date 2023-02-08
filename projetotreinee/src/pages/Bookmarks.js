f35d63f8ed85831d89d053ce6a0843d618e6a68f
Matheus Salermo <matheussalermo11@gmail.com>
2023-02-08 16:04:10 -0300

    Bookmarks, explore e menu lateral modificados

--- projetotreinee/src/pages/Bookmarks.js
+++ projetotreinee/src/pages/Bookmarks.js
@@ -6,8 +6,27 @@ import Tweet from "../components/Tweet/Tweet.js"
 import users from "../mockData/users.json";
 import tweetsDB from "../mockData/tweetsDB.json";
 import MenuMobile from "../components/MenuMobile/menuMobile";
+import RetweetTag from "../components/utilities/RetweetTag";
+import QuemSeguir from "../components/QuemSeguir/QuemSeguir";
+import Tweetar from "../components/Tweetar/tweetar"
 
 export default function BookmarksPage() {
+        const tweetsRecomendados = [...users["root"].tweetsFollowing];
+        const listaTweets = tweetsRecomendados.map(tweetId => {
+            const usuario = users[tweetsDB[tweetId].writerId];
+            const dados = {...tweetsDB[tweetId], 
+                "nome": usuario.nome,
+                "fotoPerfil": usuario.fotoPerfil,
+                "fotoPerfilRoot": users["root"].fotoPerfil,
+                "tweetId": tweetId
+    
+            }
+            return <Tweet 
+                key={tweetId}
+                {...dados}
+            />
+        });
+
     return (
         <body>
             <Header 
@@ -16,22 +35,18 @@ export default function BookmarksPage() {
                 fotoPerfil={users["root"].fotoPerfil}
             />
 
-                <div className="pagina-principal">
-                    <Menulateral 
-                        primeiro="Tweets"
-                        segundo="Tweets & replies"
-                        terceiro="Media"
-                        quarto="Likes"
-                    />
-                    <div className="tweet-tamanho">
-                        <Tweet />
-                        <Tweet />
-                        <Tweet />
-                    </div>
+                <div className="corpo-principal-home">
+                <div className="tweet-tamanho-home">
+                    <Tweetar 
+                    fotoPerfil={users["root"].fotoPerfil}/>
+                    <RetweetTag nome="Daniela Jensen" />
+                    {listaTweets}
+                </div>
                     <MenuMobile
                         tipo="bookmarks"
                     />
                 </div>
+                
         </body>
     )
 }
--- projetotreinee/src/pages/Explore.js
+++ projetotreinee/src/pages/Explore.js
@@ -7,11 +7,31 @@ import Menulateral from '../components/Menulateral/Menulateral';
 import SearchBar from '../components/SearchBar/SearchBar';
 import Tweet from '../components/Tweet/Tweet';
 import users from "../mockData/users.json";
-import tweetsDB from "../mockData/tweetsDB.json";
 import MenuMobile from "../components/MenuMobile/menuMobile";
+import tweetsDB from "../mockData/tweetsDB.json";
+import Tweetar from "../components/Tweetar/tweetar"
+import RetweetTag from "../components/utilities/RetweetTag";
+
 
 export default function explore (props) {
+    const tweetsRecomendados = [...users["root"].tweetsFollowing];
+    const listaTweets = tweetsRecomendados.map(tweetId => {
+        const usuario = users[tweetsDB[tweetId].writerId];
+        const dados = {...tweetsDB[tweetId], 
+            "nome": usuario.nome,
+            "fotoPerfil": usuario.fotoPerfil,
+            "fotoPerfilRoot": users["root"].fotoPerfil,
+            "tweetId": tweetId
+
+        }
+        return <Tweet 
+            key={tweetId}
+            {...dados}
+        />
+    });
+
     return (
+        
         <body>
             <Header 
                 tipo="explore"
@@ -27,9 +47,14 @@ export default function explore (props) {
                 />
                 <div className="menuPrincipalExplore">
                     <SearchBar />
-                    <Tweet />
-                    <Tweet />
-                    <Tweet />
+                    <div className="corpo-principal-home">
+                        <div className="tweet-tamanho-home">
+                        <Tweetar 
+                        fotoPerfil={users["root"].fotoPerfil}/>
+                        <RetweetTag nome="Daniela Jensen" />
+                        {listaTweets}
+                    </div>
+                    </div>
                 </div>
                 <MenuMobile
                     tipo="explore"
