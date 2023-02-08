407dc4a6171179070f22ee9dcf8a123624d4642c
Lucas Ferracin <lucastferracin@gmail.com>
2023-02-07 22:30:27 -0300

    mais alguns acabamentos

--- projetotreinee/src/components/CardPerfil/CapaDisplay.js
+++ projetotreinee/src/components/CardPerfil/CapaDisplay.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "../../styles/cardperfil.css"
-import capa from "./images/cardPerfil/capaTeste.jpg"
 
 export default function CapaDisplay(props) {
 
--- projetotreinee/src/components/Tweetar/tweetar.js
+++ projetotreinee/src/components/Tweetar/tweetar.js
@@ -1,34 +1,57 @@
-import React from "react";
-import "../../styles/Tweetar.css"
+import React, { useEffect, useRef, useState } from "react";
+import WhoCanReply from "../WhoCanReply/WhoCanReply.js";
+import "../../styles/Tweetar.css";
 
 export default function Tweetar(props) {
+  const imagemPerfil = require(`../../images/fotoPerfil/${props.fotoPerfil}`);
+  const [active, setActive] = useState(false);
 
-    const imagemPerfil = require(`../../images/fotoPerfil/${props.fotoPerfil}`);
+  const options = [
+    {
+      label: "Everyone",
+      value: "everyone",
+      icon: "fa-earth-americas",
+    },
+    {
+      label: "People you follow",
+      value: "followers",
+      icon: "fa-user-group",
+    },
+  ];
+  const [option, setOption] = useState(options[0]);
 
-    return (
-        <div className= "tweetar">
-            <div class="feeds-content">
-                <div class="feeds-header"> 
-                    <div class="header-top">
-                        <h4>Tweet Something</h4>
-                    </div>
-                </div>
-            </div>
-            <div className="header-post">
-                <div class="header-img-wrapper">
-                    <img src={imagemPerfil}/> 
-                    <input type="text" placeholder="What's happening?"/>
-                </div>
-            </div>
-            <div className="bottom-part">
-                <div className="area-icone-tweetar">
-                    <i className="fa-regular fa-image"></i><span></span>
-                    <div className="everyonecanreply">
-                        <i className="fa-solid fa-earth-americas"></i> <span> Everyone can reply</span>
-                    </div>
-                </div>
-                <button>Tweet</button>
-            </div>    
-         </div>     
-   );
+  function handleClick() {
+    setActive(!active);
+  }
+
+  return (
+    <div className="tweetar">
+      <div class="feeds-content">
+        <div class="feeds-header">
+          <div class="header-top">
+            <h4>Tweet Something</h4>
+          </div>
+        </div>
+      </div>
+      <div className="header-post">
+        <div class="header-img-wrapper">
+          <img src={imagemPerfil} />
+          <input type="text" placeholder="What's happening?" />
+        </div>
+      </div>
+      <div className="bottom-part">
+        <div className="area-icone-tweetar">
+          <i className="fa-regular fa-image"></i>
+          <span></span>
+          <div className="everyonecanreply" onClick={handleClick}>
+            <i className={`fa-solid ${option.icon}`}></i><span>{option.label}</span>
+          </div>
+        </div>
+        <button>Tweet</button>
+      </div>
+      <div className="who-can-reply" onClick={handleClick}>
+        {active === true && <WhoCanReply setOption={setOption} options={options} />}
+      </div>
+    </div>
+  );
 }
